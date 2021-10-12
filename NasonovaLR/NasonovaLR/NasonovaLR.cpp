766a4ccc536c4cd52c8c25233e52d84111c61237
AnastasiaNasonova <nastya.nasonova0628@gmail.com>
2021-10-12 13:01:14 +0300

    Релиз ЛР1

--- NasonovaLR/NasonovaLR/NasonovaLR.cpp
+++ NasonovaLR/NasonovaLR/NasonovaLR.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "NasonovaLR.h"
 
 using namespace std;
 
@@ -20,9 +19,9 @@ struct Pipe
 
 struct CS 
 {
-    int id, numA;
+    int id;
     string name;
-    int numW;
+    int numW, numA;
 };
 
 int GetInt()
@@ -35,8 +34,8 @@ int GetInt()
         if (cin.fail())
         {
             cin.clear();
-            cin.ignore(32767, '\n');
-            cout << "Enter a variable int";
+            cin.ignore(256, '\n');
+            cout << "Enter a variable int ";
         }
         else
         {
@@ -51,11 +50,29 @@ float GetFloat()
         float number;
         cin >> number;
 
-        if (cin.fail() || number < 0)
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(256, '\n');
+            cout << "Enter a variable float ";
+        }
+        else
+        {
+            return number;
+        }
+    }
+}
+bool GetBool()
+{
+    while (1)
+    {
+        bool number;
+        cin >> number;
+        if (cin.fail())
         {
             cin.clear();
             cin.ignore(32767, '\n');
-            cout << "Incorrect. Enter the float number more than 0: ";
+            cout << " Enter 1 or 0 ";
         }
         else
         {
@@ -65,17 +82,24 @@ float GetFloat()
 }
 
 
-void PrintPipe(Pipe& p)
+void PrintPipe( const Pipe& p)
 {
-    cout << "Pipe " << endl << endl;
-    cout << "Pipe ID number: " << p.id << "  Diameter: " << p.d << " mm " << " Lenght: " << p.l << " km\n";
-    cout << "Pipe under repair: " << p.r << endl << endl;
+    if (p.id > 0) {
+        cout << "Pipe " << endl << endl;
+        cout << "Pipe ID number: " << p.id << "  Diameter: " << p.d << " mm " << " Lenght: " << p.l << " km\n";
+        cout << "Pipe under repair: " << p.r << endl << endl; \
+    }
+    else cout << "No data pipe" << endl;
 }
 
-void PrintCS(CS& c)
+
+void PrintCS( const CS& c)
 {
-    cout << "Compressor Station " << endl << endl;
-    cout << "CS ID number: "<< c.id << " Name of the Compressor Station: " << c.name << " Number of workshops: " << c.numA << " Number of workshops in olperation: " << c.numW;
+    if (c.id > 0) {
+        cout << "Compressor Station " << endl << endl;
+        cout << "CS ID number: " << c.id << " Name of the Compressor Station: " << c.name << " Number of workshops: " << c.numA << " Number of workshops in olperation: " << c.numW;
+    }
+    else cout << "No data CS" << endl;
 }
 
 Pipe AddPipe()
@@ -92,14 +116,12 @@ Pipe AddPipe()
     }
     cout << "Enter the lenght ";
     p.l = GetFloat();
-    while (p.l < 10 || p.l > 100) {
-        cout << "The length should be from 10 to 100 km, enter a lenght: ";
+    while (p.l < 10 || p.l > 1000) {
+        cout << "The length should be from 10 to 1000 km, enter a lenght: ";
         p.l = GetFloat();
     }
     p.r = 0;
-    //cout << "Is the pipe being repaired ? ";
-    //cin >> p.r;
-  
+      
     return p;
 }
 CS AddCS()
@@ -107,45 +129,51 @@ CS AddCS()
     CS c;
     c.id = 1;
     cout << "Name of the Compressor Station ";
-    cin >> c.name;
+    cin >> ws;
+    getline(cin, c.name);
     cout << "Number of workshops ";
     c.numA = GetInt();
-    while (c.numA > 20) {
+    while (c.numA > 20 || c.numA < 0) {
         cout << "Workshops should be at least 1, but less then 20, enter a number of workshops ";
-        cin >> c.numA;
+        c.numA = GetInt();
     }
     cout << "Number of workshops in olperation ";
     c.numW = GetInt();
     while (c.numW > c.numA) {
         cout << "The number of workstation must not exceed the number of all station " << endl << "Enter another number ";
-        cin >> c.numW;
+        c.numW = GetInt();
     };
     return c;
 }
 
-Pipe RedactPipe(Pipe& p) {
-    if (p.id == 1) {
+void RedactPipe(Pipe& p) 
+{
+        if (p.id > 0) {
         cout << "Enter the state of the pipe (1 - under repair; 0 - not under repair)";
-        cin >> p.r;
-    }
-    else cout << "No pipe data available"; //данных о трубе нет
-    return p;
-}
-CS RedactCS(CS& c) {
+        p.r = GetBool();
 
-    if (c.id == 1) {
+        }
+    else cout << "No pipe data available"; //данных о трубе нет
+}
+void RedactCS(CS& c) {
+
+    if (c.id > 0) {
         cout << "How many workshops are in operation? ";
         c.numW = GetInt();
+
         while (c.numW > c.numA) {
+            cout << "The number of workstation must not exceed the number of all station " << endl << "Enter another number";
+            c.numW = GetInt();
+        };
+        while (c.numW < 0) {
             cout << "The number of workstation must not exceed the number of all station " << endl << "Enter another number";
             c.numW = GetInt();
         };
     }
     else cout << "No CS data available";
-    return c;
 }
 
-void Save(Pipe& p, CS& c)
+void Save( const Pipe& p, const CS& c)
 {
     ofstream f;
     f.open("save.txt", ios_base::out);
@@ -154,33 +182,19 @@ void Save(Pipe& p, CS& c)
     {
         if (p.id > 0)
         {
-            f << "pipe" << endl
-                << p.id << endl
-                << p.l << endl
-                << p.d << endl
-                << p.r << endl;
-        }
-        else
-        {
-            cout << "No pipe";
+            f << "pipe" << endl << p.id << endl << p.l << endl << p.d << endl << p.r << endl;
         }
 
         if (c.id > 0)
         {
-            f << "station" << endl
-                << c.id << endl
-                << c.name << endl
-                << c.numA << endl
-                << c.numW << endl;
-            //<< station.effect << endl;
-        }
-        else
-        {
-            cout << "No Station" << endl << endl;
+            f << "station" << endl << c.id << endl << c.name << endl << c.numA << endl << c.numW << endl;
+            
         }
         f.close();
+        cout << "Saved";
 
     }
+    else cout << "No pipe or station";
 
 }
 
@@ -189,39 +203,32 @@ void Load(Pipe& p, CS& c)
     ifstream f;
     f.open("save.txt", ios::in);
     if (f.good())
-    {
-      
+    {      
             while (!f.eof())
             {
                 string str;
-                getline(f, str);
+                f >> str;
+                //cout << str;
                 if (str == "pipe")
                 {
-                    getline(f, str);
-                    p.id = stoi(str);
-                    getline(f, str);
-                    p.l = stof(str);
-                    getline(f, str);
-                    p.d = stoi(str);
-                    getline(f, str);
-                    p.r = (str == "1");
+                    f >> p.id;
+                    f >> p.d;
+                    f >> p.l;
+                    f >> p.r;  
                 }
-
+              
                 if (str == "station")
                 {
-                    string str;
-                    getline(f, str);
-                    c.id = stoi(str);
-                    getline(f, str);
-                    c.name = str;
-                    getline(f, str);
-                    c.numA = stoi(str);
-                    getline(f, str);
-                    c.numW = stoi(str);
-                    getline(f, str);
-                }
-            cout << "Loaded." << endl << endl;
-        }
+                    f >> c.id;
+                    f.ignore(256, '\n');
+                    string(c.name);
+                    getline(f, c.name);
+                    f >> c.numA;
+                    f >> c.numW;
+                }              
+            
+            }
+            cout << "Loaded";
     }
 }
 
@@ -233,11 +240,8 @@ int main()
     CS c = {};
     
     for (;;) {
-    cout << "\n 1. Add a pipe\n 2. Add Compressor Station\n 3. Viewing all objects\n 4. Redact Pipe\n 5. Redact CS\n 6. Save\n 7.Load\n 0. Exit\n";
-    //while (menu > 5 || menu < 0) {
-      //  cout << "There is no such number in the menu, enter another number";
-      //  cin >> menu;
-
+    cout << "\n 1. Add a pipe\n 2. Add Compressor Station\n 3. Viewing all objects\n 4. Redact Pipe\n 5. Redact CS\n 6. Save\n 7. Load\n 0. Exit\n";
+    
     menu = GetInt();
     
         switch (menu)
@@ -245,21 +249,31 @@ int main()
         case 0:
             return 0;
         case 1:
+        {
             p = AddPipe();
             break;
+        }
         case 2:
+        {
             c = AddCS();
             break;
+        }
         case 3:
+        {
             PrintPipe(p);
             PrintCS(c);
             break;
+        }
         case 4:
+        {
             RedactPipe(p);
             break;
+        }
         case 5:
+        {
             RedactCS(c);
             break;
+        }
         case 6:
         {
             Save(p, c);
